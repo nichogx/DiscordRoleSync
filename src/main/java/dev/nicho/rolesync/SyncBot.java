nichogx/DiscordRoleSync

commit b42704694db927935bf77f3a666cb5a863d2653f

Don't execute blocking operations on the server thread
These changes ensure that blocking, or potentially blocking, operations
are always executed on a thread other than the main server thread.
Executing blocking operations on the main server thread will cause
noticeable lag issues.

src/main/java/dev/nicho/rolesync/db/DatabaseHandler.java
@@ -1,5 +1,6 @@
 package dev.nicho.rolesync.db;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.security.SecureRandom;
@@ -80,6 +81,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public int getLinkedUserCount() throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM "
                 + plugin.getConfig().getString("database.tablePrefix") + "_discordmcusers");
@@ -101,6 +104,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public void linkUser(String discordID, String minecraftUUID) throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "INSERT INTO " + plugin.getConfig().getString("database.tablePrefix") +
@@ -126,6 +131,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public void addToWhitelist(String uuid) throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "UPDATE " + plugin.getConfig().getString("database.tablePrefix") + "_discordmcusers " +
@@ -145,6 +152,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public void removeFromWhitelist(String uuid) throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "UPDATE " + plugin.getConfig().getString("database.tablePrefix") + "_discordmcusers " +
@@ -164,6 +173,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public void unlink(String uuid) throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement("DELETE FROM "
                 + plugin.getConfig().getString("database.tablePrefix") + "_discordmcusers WHERE minecraft_uuid = ?");
@@ -184,6 +195,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public void forAllLinkedUsers(Consumer<LinkedUserInfo> callback) throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement("SELECT discord_id, minecraft_uuid, whitelisted, verification_code, verified, username_when_linked FROM "
                 + plugin.getConfig().getString("database.tablePrefix") + "_discordmcusers");
@@ -211,6 +224,8 @@ public abstract class DatabaseHandler {
      * @throws SQLException if an SQL error occurs
      */
     public LinkedUserInfo getLinkedUserInfo(String identifier) throws SQLException {
+        checkAsync();
+
         Connection c = this.getConnection();
         PreparedStatement ps = c.prepareStatement("SELECT discord_id, minecraft_uuid, whitelisted, verification_code, verified, username_when_linked FROM "
                 + plugin.getConfig().getString("database.tablePrefix") + "_discordmcusers "
@@ -339,6 +354,12 @@ public abstract class DatabaseHandler {
         }
 
         return migrated;
+    }
+
+    private final void checkAsync() {
+        if (Bukkit.isPrimaryThread()) {
+            throw new IllegalStateException("Attempted to execute a database operation from the server thread!");
+        }
     }
 
     /**

.gitignore
@@ -1,3 +1,4 @@
 lib
 .idea
+*.iml
 target

src/main/java/dev/nicho/rolesync/RoleSync.java
@@ -31,6 +31,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 public class RoleSync extends JavaPlugin {
 
@@ -134,11 +135,6 @@ public class RoleSync extends JavaPlugin {
         } catch (IOException | SQLException e) {
             getLogger().severe("Error setting up database");
             e.printStackTrace();
-            this.setEnabled(false);
-
-            return;
-        } catch (LoginException e) {
-            getLogger().severe("Error logging in. Did you set your token in config.yml?");
             this.setEnabled(false);
 
             return;
@@ -279,38 +275,32 @@ public class RoleSync extends JavaPlugin {
 
             jda.shutdown();
 
-            try {
-                startBot();
-                sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.GREEN + language.getString("botRestarted"));
-            } catch (LoginException e) {
-                e.printStackTrace();
-
-                return false;
-            }
+            startBot();
+            sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.GREEN + language.getString("botRestarted"));
 
             return true;
         } else if (args[0].equalsIgnoreCase("verify")) {
             if (sender instanceof Player) {
-                try {
-                    DatabaseHandler.LinkedUserInfo userInfo = db.getLinkedUserInfo(((Player) sender).getUniqueId().toString());
-                    if (userInfo == null) {
-                        sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RESET + language.getString("pleaseLink")
-                                + " " + getConfig().getString("discordUrl"));
-                    } else if (!userInfo.verified) {
-                        sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RESET + language.getString("verificationInstructions")
-                                + " " + ChatColor.AQUA + userInfo.code);
-                    } else {
-                        sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RESET + language.getString("alreadyVerified"));
+                this.getServer().getScheduler().runTaskAsynchronously(this, () -> {
+                    try {
+                        DatabaseHandler.LinkedUserInfo userInfo = db.getLinkedUserInfo(((Player) sender).getUniqueId().toString());
+                        if (userInfo == null) {
+                            sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RESET + language.getString("pleaseLink")
+                                    + " " + getConfig().getString("discordUrl"));
+                        } else if (!userInfo.verified) {
+                            sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RESET + language.getString("verificationInstructions")
+                                    + " " + ChatColor.AQUA + userInfo.code);
+                        } else {
+                            sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RESET + language.getString("alreadyVerified"));
+                        }
+                    } catch (SQLException e) {
+                        sender.sendMessage(ChatColor.RED + language.getString("commandError"));
+                        getLogger().severe("An error occurred while getting linked user info. Please check the stack trace below and contact the developer.");
+                        e.printStackTrace();
                     }
+                });
 
-                    return true;
-                } catch (SQLException e) {
-                    sender.sendMessage(ChatColor.RED + language.getString("commandError"));
-                    getLogger().severe("An error occurred while getting linked user info. Please check the stack trace below and contact the developer.");
-                    e.printStackTrace();
-
-                    return false;
-                }
+                return true;
             } else {
                 sender.sendMessage(ChatColor.BLUE + "[DRS] " + ChatColor.RED + "This command can only be used in game.");
 
@@ -351,22 +341,31 @@ public class RoleSync extends JavaPlugin {
         return langFile;
     }
 
-    private void startBot() throws LoginException {
-        getLogger().info("Initializing bot");
-        JDABuilder builder = JDABuilder
-                .create(getConfig().getString("botInfo.token"),
-                        GatewayIntent.GUILD_MEMBERS,
-                        GatewayIntent.GUILD_MESSAGES,
-                        GatewayIntent.DIRECT_MESSAGES,
-                        GatewayIntent.GUILD_BANS)
-                .disableCache(
-                        CacheFlag.EMOTE,
-                        CacheFlag.VOICE_STATE,
-                        CacheFlag.ACTIVITY,
-                        CacheFlag.CLIENT_STATUS
-                );
+    private void startBot() {
+        this.getServer().getScheduler().runTaskAsynchronously(this, () -> {
+            try {
+                getLogger().info("Initializing bot");
+                JDABuilder builder = JDABuilder
+                        .create(getConfig().getString("botInfo.token"),
+                                GatewayIntent.GUILD_MEMBERS,
+                                GatewayIntent.GUILD_MESSAGES,
+                                GatewayIntent.DIRECT_MESSAGES,
+                                GatewayIntent.GUILD_BANS)
+                        .disableCache(
+                                CacheFlag.EMOTE,
+                                CacheFlag.VOICE_STATE,
+                                CacheFlag.ACTIVITY,
+                                CacheFlag.CLIENT_STATUS
+                        );
 
-        builder.addEventListeners(listener);
-        this.jda = builder.build();
+                builder.addEventListeners(listener);
+                this.jda = builder.build();
+            } catch (LoginException e) {
+                getLogger().log(Level.SEVERE, "Error logging in. Did you set your token in config.yml?", e);
+
+                // Switch back to main thread to disable ourselves
+                this.getServer().getScheduler().runTask(this, () -> this.setEnabled(false));
+            }
+        });
     }
 }

src/main/java/dev/nicho/rolesync/listeners/PlayerJoinListener.java
@@ -1,6 +1,5 @@
 package dev.nicho.rolesync.listeners;
 
-import dev.nicho.rolesync.util.MojangAPI;
 import dev.nicho.rolesync.util.Util;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -8,14 +7,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.json.JSONArray;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
 
 public class PlayerJoinListener implements Listener {
 
@@ -30,24 +23,26 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (event.getPlayer().hasPermission("discordrolesync.notifyupdates")) {
-            // check for updates and send if available
-            String version = plugin.getDescription().getVersion();
-            String latestVersion;
-            try {
-                latestVersion = Util.getLatestVersion();
-            } catch (IOException e) {
-                e.printStackTrace();
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+                // check for updates and send if available
+                String version = plugin.getDescription().getVersion();
+                String latestVersion;
+                try {
+                    latestVersion = Util.getLatestVersion();
+                } catch (IOException e) {
+                    e.printStackTrace();
 
-                return;
-            }
+                    return;
+                }
 
-            if (!latestVersion.equalsIgnoreCase(version)) {
-                String message = ChatColor.BLUE + "[DRS] " + ChatColor.AQUA + lang.getString("notLatestVersion") + "\n" +
-                        ChatColor.BLUE + "[DRS] " + ChatColor.AQUA + lang.getString("current") + " " + ChatColor.RED + version + ChatColor.AQUA + "\n" +
-                        ChatColor.BLUE + "[DRS] " + ChatColor.AQUA + lang.getString("latest") + " " + ChatColor.GREEN + latestVersion;
+                if (!latestVersion.equalsIgnoreCase(version)) {
+                    String message = ChatColor.BLUE + "[DRS] " + ChatColor.AQUA + lang.getString("notLatestVersion") + "\n" +
+                            ChatColor.BLUE + "[DRS] " + ChatColor.AQUA + lang.getString("current") + " " + ChatColor.RED + version + ChatColor.AQUA + "\n" +
+                            ChatColor.BLUE + "[DRS] " + ChatColor.AQUA + lang.getString("latest") + " " + ChatColor.GREEN + latestVersion;
 
-                event.getPlayer().sendMessage(message);
-            }
+                    event.getPlayer().sendMessage(message);
+                }
+            });
         }
     }
 }

src/main/java/dev/nicho/rolesync/listeners/WhitelistLoginListener.java
@@ -6,7 +6,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
@@ -23,13 +23,13 @@ public class WhitelistLoginListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler (priority = EventPriority.HIGHEST)
-    public void onPlayerLogin(PlayerLoginEvent event) {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLogin(AsyncPlayerPreLoginEvent event) {
         DatabaseHandler.LinkedUserInfo usrInfo;
         try {
-            usrInfo = db.getLinkedUserInfo(event.getPlayer().getUniqueId().toString());
+            usrInfo = db.getLinkedUserInfo(event.getUniqueId().toString());
         } catch (SQLException e) {
-            event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, lang.getString("whitelistErrorKickMsg"));
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, lang.getString("whitelistErrorKickMsg"));
             plugin.getLogger().severe("Error while checking if a user is whitelisted. Please check the stack trace below and contact the developer if needed.");
             e.printStackTrace();
 
@@ -38,13 +38,13 @@ public class WhitelistLoginListener implements Listener {
 
         if (usrInfo == null) {
             // user not linked
-            event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, lang.getString("pleaseLink")
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, lang.getString("pleaseLink")
                     + " " + plugin.getConfig().getString("discordUrl"));
         } else if (plugin.getConfig().getBoolean("requireVerification") && !usrInfo.verified) {
-            event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, lang.getString("whitelistNotVerifiedKickMsg")
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, lang.getString("whitelistNotVerifiedKickMsg")
                     + " " + ChatColor.AQUA + usrInfo.code);
         } else if (!usrInfo.whitelisted) {
-            event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, lang.getString("notWhitelistedKickMsg"));
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, lang.getString("notWhitelistedKickMsg"));
         }
     }
 }

commit 8b571217f987e9d134799eb8eec3863d00aa67b0

fix bug introduced in b427046
when database accesses were made async, linking users that already had a role stopped working. This fixes that.

pom.xml
@@ -6,7 +6,7 @@
 
     <groupId>dev.nicho.rolesync</groupId>
     <artifactId>discord_role_sync</artifactId>
-    <version>1.0.0-BETA.15</version>
+    <version>1.0.0-BETA.16</version>
 
     <properties>
         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>

src/main/java/dev/nicho/rolesync/SyncBot.java
@@ -102,7 +102,6 @@ public class SyncBot extends ListenerAdapter {
             ch.info(argv, event);
         } else if (argv[0].equalsIgnoreCase("link")) {
             ch.link(argv, event);
-            checkMemberRoles(event.getMember());
         } else if (argv[0].equalsIgnoreCase("unlink")) {
             ch.unlink(argv, event);
         }
@@ -370,7 +369,9 @@ public class SyncBot extends ListenerAdapter {
                     return;
                 }
 
-                db.linkUser(event.getAuthor().getId(), uuid);
+                db.linkUser(event.getAuthor().getId(), uuid, () -> {
+                    checkMemberRoles(event.getMember());
+                });
                 if (!plugin.getConfig().getBoolean("requireVerification")) giveRoleAndNickname(Objects.requireNonNull(event.getMember()), result.name);
                 JDAUtils.reactAndDelete(plugin.getConfig().getString("react.onSuccess"), event.getMessage(), plugin.getConfig());
             } catch (SQLException | IOException e) {

src/main/java/dev/nicho/rolesync/db/DatabaseHandler.java
@@ -103,7 +103,7 @@ public abstract class DatabaseHandler {
      * @param minecraftUUID the Minecraft UUID of the user
      * @throws SQLException if an SQL error occurs
      */
-    public void linkUser(String discordID, String minecraftUUID) throws SQLException {
+    public void linkUser(String discordID, String minecraftUUID, Runnable callback) throws SQLException {
         checkAsync();
 
         Connection c = this.getConnection();
@@ -122,6 +122,8 @@ public abstract class DatabaseHandler {
         ps.execute();
 
         this.closeConnection(c);
+
+        callback.run();
     }
 
     /**

src/main/resources/plugin.yml
@@ -1,5 +1,5 @@
 name: DiscordRoleSync
-version: 1.0.0-BETA.15
+version: 1.0.0-BETA.16
 author: Nicho
 description: Sync Discord roles with the server's whitelist and permission plugins.
 main: dev.nicho.rolesync.RoleSync