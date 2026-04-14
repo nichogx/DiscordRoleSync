3d01860cfa22075ab1ae8e859b3fa54fb911e7f9
Nicholas Xavier <nicho@nicho.dev>
2026-04-14 00:04:58 -0700

    update supported versions

--- ci/publish_hangar.py
+++ ci/publish_hangar.py
@@ -10,9 +10,9 @@ from common import (
 )
 
 SUPPORTED_GAME_VERSIONS = [
-    "1.8", # Hangar does not list all 1.8 versions
+    "1.8", "1.8.8", # Hangar does not list all 1.8 versions
     "1.9.x", "1.10.x", "1.11.x", "1.12.x", "1.13.x", "1.14.x", "1.15.x", "1.16.x", "1.17.x",
-    "1.18.x", "1.19.x", "1.20.x", "1.21.x",
+    "1.18.x", "1.19.x", "1.20.x", "1.21.x", "26.1.x",
 ]
 
 if __name__ == "__main__":
